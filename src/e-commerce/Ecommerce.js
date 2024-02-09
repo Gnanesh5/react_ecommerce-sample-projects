9723d969df2495580e211102dc7512ecd4d0cfe1
Gnanesh5 <gkrishnapatnam5100@sdsu.edu>
2024-02-08 23:22:24 -0800

    Modifications done with the filtering the products

--- src/e-commerce/Ecommerce.js
+++ src/e-commerce/Ecommerce.js
@@ -1,21 +1,88 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navigation from './navigation/Navigation';
 import Products from './products/Products';
 import Sidebar from './sidebar/Sidebar';
 import Recommended from './recommended/Recommended';
 
+import Product from './db/data'
+import Card from './components/Card'
+
 
 import '../Style.css'
 
-const Ecommerce = () => {
-  return (
-    <>
-        <Sidebar />
-        <Navigation />
-        <Products />
-        <Recommended />
-    </>
-  )
-}
+function Ecommerce() {
+    const [selectedCategory, setSelectedCategory] = useState(null);
+  
+    // ----------- Input Filter -----------
+    const [query, setQuery] = useState("");
+  
+    const handleInputChange = (event) => {
+      setQuery(event.target.value);
+    };
+  
+    const filteredItems = Product.filter(
+      (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    );
+  
+    // ----------- Radio Filtering -----------
+    const handleChange = (event) => {
+      setSelectedCategory(event.target.value);
+    };
+  
+    // ------------ Button Filtering -----------
+    const handleClick = (event) => {
+      setSelectedCategory(event.target.value);
+    };
+  
+    function filteredData(products, selected, query) {
+      let filteredProducts = Product;
+  
+      // Filtering Input Items
+      if (query) {
+        filteredProducts = filteredItems;
+      }
+  
+      // Applying selected filter
+      if (selected) {
+        filteredProducts = filteredProducts.filter(
+          ({ category, color, company, newPrice, title }) =>
+            category === selected ||
+            color === selected ||
+            company === selected ||
+            newPrice === selected ||
+            title === selected
+        );
+      }
+  
+      const data = filteredProducts.map(
+        ({ img, title, star, reviews, prevPrice, newPrice }) => (
+          <Card
+            key={Math.random()}
+            img={img}
+            title={title}
+            star={star}
+            reviews={reviews}
+            prevPrice={prevPrice}
+            newPrice={newPrice}
+          />
+        )
+      );
 
-export default Ecommerce
+      return data
+
+    }
+
+  
+    const result = filteredData(Products, selectedCategory, query);
+  
+    return (
+      <>
+        <Sidebar handleChange={handleChange} />
+        <Navigation query={query} handleInputChange={handleInputChange} />
+        <Recommended handleClick={handleClick} />
+        <Products result={result} />
+      </>
+    );
+  }
+
+  export default Ecommerce
--- src/e-commerce/sidebar/Sidebar.js
+++ src/e-commerce/sidebar/Sidebar.js
@@ -8,7 +8,7 @@ import { TiShoppingCart } from "react-icons/ti";
 
 import './Sidebar.css'
 
-const Sidebar = () => {
+const Sidebar = (handleChange) => {
   return (
     <>
       <section className='sidebar'>
@@ -16,9 +16,9 @@ const Sidebar = () => {
           <h1><TiShoppingCart /></h1>
         </div>
 
-        <Category />
-        <Price />
-        <Colors />
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        <Colors handleChange={handleChange} />
 
       </section>
     </>
